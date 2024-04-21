8d2b3401a90c50e81efba1bd6bcc0fbf537f8d5b
Damiil Shirokov <daniil196196@gmail.com>
2024-04-21 18:40:12 +0500

    reformat code

--- src/Calculator.java
+++ src/Calculator.java
@@ -5,18 +5,16 @@ public class Calculator {
     static Supplier<Calculator> instance = Calculator::new;
 
 
-
     BinaryOperator<Integer> plus = (x, y) -> x + y;
     BinaryOperator<Integer> minus = (x, y) -> x - y;
     BinaryOperator<Integer> multiply = (x, y) -> x * y;
     BinaryOperator<Integer> devide = (x, y) -> x / y;
 
-    Predicate<Integer> isPositive= x -> x > 0;
+    Predicate<Integer> isPositive = x -> x > 0;
 
     Predicate<Integer> isEqualZero = x -> x == 0;
     UnaryOperator<Integer> pow = x -> x * x;
     UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
-
 
 
     Consumer<Integer> println = System.out::println;
